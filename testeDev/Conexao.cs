4f7205f17ba0734c3242a3da7e5df7a50f3bb52e
joao <jv.anjos98@gmail.com>
2021-01-05 19:23:56 -0300

    Adicionar arquivos de projeto.

---
+++ testeDev/Conexao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testeDev
+{
+    public class Conexao
+    {
+        public string conec = "SERVER=localhost; DATABASE=testeDev; UID=root; PWD=usuario; PORT=3306;";
+        public MysqlConnection con = null;
+    }
+}
